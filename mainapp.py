10a02678ee419cc22940df472020da13e0c6f8c7
Yohikodayo <yohikodayo@gmail.com>
2025-01-22 21:11:20 +0800

    20250122--shiyan 1. 先上傳目前進度

---
+++ mainapp.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+# click element -> element.click()
+# input text -> element.send_keys()
+# get text -> element.text
+# get attribute -> element.get_attribute()
+# get css value -> element.value_of_css_property()
+# get tag name -> element.tag_name
+# find element -> driver.find_element(By.CSS_SELECTOR, "selector")
+# close driver -> driver.quit()
+
+
+# 初始化瀏覽器
+options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")  # 避免權限問題
+options.add_argument("--disable-dev-shm-usage")  # 增加對大數據的支援
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+url = "https://itouch.cycu.edu.tw/home/#/ann"
+driver.get(url)
+
+time.sleep(1)
+
+# todo 進行操作
+id_input_ele = driver.find_elements(By.CSS_SELECTOR, "[name='']")
+pwd_input_ele = driver.find_elements(By.CSS_SELECTOR, "[name='']")
+
